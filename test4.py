e12c2390fe385f22d475c94b6b862c9dc5a76c5f
Administrator <some_email@gmail.com>
2021-06-15 12:45:55 +0100

    update test4 VPN

--- test4.py
+++ test4.py
@@ -1,19 +1,107 @@
-import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+class FireFoxDriverWithProxy:
+    def __init__(self):
+        firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = True
+
+        fp = webdriver.FirefoxProfile(
+            r'C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\wxjvqcon.default-release-1')
+
+        options = webdriver.FirefoxOptions()
+        options.set_preference("dom.webdriver.enabled", False)
+        options.set_preference("dom.webnotifications.enabled", False)
+        binary = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        options.binary = binary
+
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'
+        options.set_preference("general.useragent.override", user_agent)
+
+        # 	Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.022
+
+        driver = webdriver.Firefox(capabilities=firefox_capabilities, firefox_profile=fp,
+                                   firefox_binary='C:/Program Files/Mozilla Firefox/firefox.exe',
+                                   executable_path=r'C:\Users\Administrator\PycharmProjects\bet1\geckodriver.exe',
+                                   options=options)
+
+        self.driver = driver
+        time.sleep(2)
+        print('go')
+
+
+        try:
+            self.driver.get('https://www.bet365.com/')
+        except:
+            pass
+
+        bad_ip = True
+        while bad_ip:
+            answer_ = self.check_ip()
+            if answer_:
+                print('[+] Stop scerch for new ip')
+                break
+
+            self.driver.quit()
+            time.sleep(2)
+            #
+            firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+            firefox_capabilities['marionette'] = True
+
+            fp = webdriver.FirefoxProfile(
+                r'C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\wxjvqcon.default-release-1')
+
+            options = webdriver.FirefoxOptions()
+            options.set_preference("dom.webdriver.enabled", False)
+            options.set_preference("dom.webnotifications.enabled", False)
+            binary = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+            options.binary = binary
+
+            user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'
+            options.set_preference("general.useragent.override", user_agent)
+            #
+            self.driver = webdriver.Firefox(capabilities=firefox_capabilities, firefox_profile=fp,
+                            firefox_binary='C:/Program Files/Mozilla Firefox/firefox.exe',
+                            executable_path=r'C:\Users\Administrator\PycharmProjects\bet1\geckodriver.exe',
+                            options=options)
+            time.sleep(2)
+
+            self.driver.get('https://www.bet365.com/')
 
 
 
-BOT_TOKEN_GRAF = '1673449920:AAEmKCmlSxzD2IxVKKKznmsawJUzYaZsKzI'
+    def check_ip(self):
+        self.driver.get('https://www.bet365.com/')
+        for i in range(3):
+            try:
+                try:
+                    time.sleep(3)
+                    self.driver.find_element_by_class_name('hm-MainHeaderRHSLoggedOutWide_LoginContainer')
+                    print('[+] Log in account with VPN')
+                    return True
+                except:
+                    print('wait...')
+            except:
+                pass
+        print('[-] Next one ...')
 
-my_id = '409524113'
+        return False
 
 
-def telegram_bot_send_photo(bot_token, chat_id, path_to_photo):
-    url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
-    files = {'photo': open(path_to_photo, 'rb')}
-    data = {'chat_id': chat_id}
-    r = requests.post(url, files=files, data=data)
-    return r.json()
-
-print(telegram_bot_send_photo(BOT_TOKEN_GRAF, my_id, '1.png'))
 
 
+
+
+
+
+
+
+d1 = FireFoxDriverWithProxy()
+d11 = FireFoxDriverWithProxy()
+d111 = FireFoxDriverWithProxy()
+d1111 = FireFoxDriverWithProxy()
+
+d1.driver.refresh()
+print('end')
--- test_open_firefox_and_chrome.py
+++ test_open_firefox_and_chrome.py
@@ -4,11 +4,18 @@ import time
 
 firefox_driver = chromdriver_class.FireFoxDriver(data.path_to_geckodriver, data.positivebet_useragent)
 print('[+] Firefox работает')
-
-chrome_driver = chromdriver_class.ChromeCloudFlareProtection()
-print('[+] Chrome работает')
+'''firefox_driver.driver.get(
+    'https://bet365.ru'
+)
+'''
+#chrome_driver = chromdriver_class.ChromeCloudFlareProtection()
+'''print('[+] Chrome работает')
 time.sleep(5)
 
 firefox_driver.close_session()
+
+
 chrome_driver.close_session()
-print('[+] The End.')
+
+print('[+] The End.')'''
+
