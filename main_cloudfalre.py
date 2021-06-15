4a113ee54c27b7dc9dd3a006f4ac7098b004eabe
ser4 <pristup2040@gmail.com>
2021-06-15 22:09:27 +1000

    add FireFoxDriverWithVPN to main_cloudfalre.py

--- main_cloudfalre.py
+++ main_cloudfalre.py
@@ -1,4 +1,4 @@
-from chromdriver_class import FireFoxDriverWithProxy, ChromeCloudFlareProtection
+from chromdriver_class import FireFoxDriverWithProxy, ChromeCloudFlareProtection, FireFoxDriverWithVPN
 import time
 import data
 import datetime
@@ -57,8 +57,15 @@ i1 = 1
 for i in range(len(data.Accounts)):
     print(f'Запуск аккаунта {i1}')
     i1+=1
-    driver2 = FireFoxDriverWithProxy(path_to_geckodriver=data.path_to_geckodriver, user_agent=data.Accounts[i][0],
+
+    # запуск аккаунта с VPN
+    if data.Accounts[i][2] == 'VPN':
+        driver2 = FireFoxDriverWithVPN(path_to_geckodriver=data.path_to_geckodriver, user_agent=data.Accounts[i][0],
                                  proxy=data.Accounts[i][1], proxy_login_and_password=data.Accounts[i][2], type_of_account=data.Accounts[i][6], final_balance=data.Accounts[i][7], account_code_name=data.Accounts[i][8], is_reversed=data.Accounts[i][9])
+    else:
+        driver2 = FireFoxDriverWithProxy(path_to_geckodriver=data.path_to_geckodriver, user_agent=data.Accounts[i][0],
+                                 proxy=data.Accounts[i][1], proxy_login_and_password=data.Accounts[i][2], type_of_account=data.Accounts[i][6], final_balance=data.Accounts[i][7], account_code_name=data.Accounts[i][8], is_reversed=data.Accounts[i][9])
+
     driver2.log_in_bet365(data.Accounts[i][3], data.Accounts[i][4], data.Accounts[i][6])
     List_of_bet_account[i] = driver2
 
